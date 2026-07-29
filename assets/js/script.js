2e7c46b0faa4afcb269c289c101f95268d9501e1
Amir Mohammadi <amirmhmdi95@gmail.com>
2026-07-29 11:13:29 +0100

    multiply, subtraction and divide function

--- assets/js/script.js
+++ assets/js/script.js
@@ -107,8 +107,20 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById("operator").textContent = "+";
  }
 
-function displaySubtractQuestion() { }
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "-";
+ }
 
-function displayMultiplyQuestion() { }
+function displayMultiplyQuestion(operand1, operand2) { 
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "x";
+}
 
-function displayDivideQuestion() { }
+function displayDivideQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
+}
