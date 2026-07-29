f14ab561ab3754b8e4edc26c59ad13739292c719
Amir Mohammadi <amirmhmdi95@gmail.com>
2026-07-29 11:29:28 +0100

    add facicon, prevent to negative in subtract, enter key and focus to the answer box

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,10 +13,20 @@ document.addEventListener("DOMContentLoaded", function () {
         })
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    });
+
     runGame("addition");
 });
 
 function runGame(gameType) {
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+    
+
     // create two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
@@ -108,8 +118,8 @@ function displayAdditionQuestion(operand1, operand2) {
  }
 
 function displaySubtractQuestion(operand1, operand2) {
-    document.getElementById("operand1").textContent = operand1;
-    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById("operator").textContent = "-";
  }
 
